abfd9c8e22d7ab33797788f055f1e2284ddba32c
mohitb07 <bmohit980@gmail.com>
2022-02-03 20:09:02 +0530

    monaco editor configured

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const withCSS = require("@zeit/next-css");
+const MonacoEditorWebpackPlugin = require("monaco-editor-webpack-plugin");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+const withTM = require("next-transpile-modules")(["monaco-editor"]);
+
+module.exports = withTM({
+  webpack: (config) => {
+    const rule = config.module.rules
+      .find((rule) => rule.oneOf)
+      .oneOf.find(
+        (r) => r.issuer && r.issuer.include && r.issuer.include.includes("_app")
+      );
+    if (rule) {
+      rule.issuer.include = [
+        rule.issuer.include,
+        /[\\/]node_modules[\\/]monaco-editor[\\/]/,
+      ];
+    }
+    config.plugins.push(
+      new MonacoEditorWebpackPlugin({ languages: ["javascript"] })
+    );
+    // config.plugins.push(
+    //   new MonacoWebpackPlugin({
+    //     language: ["javascript", "typescript", "markdown"],
+    //     filename: "static/[name].worker.js",
+    //   })
+    // );
+    return config;
+  },
+});
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
 import Head from "next/head";
+import dynamic from "next/dynamic";
+
+const MonacoEditor = dynamic(import("react-monaco-editor"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="container">
+    <div style={{ height: "100vh", width: "100vw" }} className="container">
       <h1>Hello world</h1>
+      <MonacoEditor
+        width={800}
+        width={600}
+        theme="vs-dark"
+        language="javascript"
+        // editorDidMount={() => {
+        //   // @ts-ignore
+        //   window.MonacoEnvironment.getWorkerUrl = (_moduleId, label) => {
+        //     if (label === "json") return "_next/static/json.worker.js";
+        //     if (label === "css") return "_next/static/css.worker.js";
+        //     if (label === "html") return "_next/static/html.worker.js";
+        //     if (label === "typescript" || label === "javascript")
+        //       return "_next/static/ts.worker.js";
+        //     return "_next/static/editor.worker.js";
+        //   };
+        // }}
+        // width="800"
+        // height="600"
+        // language="markdown"
+        // theme="vs-dark"
+        // value="fasdfs"
+        // options={{
+        //   minimap: {
+        //     enabled: false,
+        //   },
+        // }}
+        // // onChange={setPostBody}
+      />
     </div>
   );
 }
